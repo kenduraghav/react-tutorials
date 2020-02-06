83305f1ba82a0c24601f5fea0a99b3d4d43f2f89
kenduraghav <kenduraghav@gmail.com>
2020-02-06 20:45:02 +0530

    #100DaysofCode - Added new parent component that wraps a children component inside it.

--- function-components/src/CardDetail.js
+++ function-components/src/CardDetail.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
-
-const CardDetail =  (props) => {
-
-    return (
-        <div className="card">
-                    <div className="content">
-                        <div className="header">
-                            {props.question}
-                        </div>
-                        <div className="description">
-                            {props.answer}
-                        </div>
-                    </div>
-                </div>
-    );
-}
-
+const CardDetail = props => {
+  return (
+    <div className='comment'>
+      <div className='content'>
+        <div className='ui dividing header'>{props.question}</div>
+        <div className='description'>
+          <p>{props.answer}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CardDetail;
--- function-components/src/index.js
+++ function-components/src/index.js
@@ -1,7 +1,8 @@
 //import react libraries
-import React from "react";
-import ReactDOM from "react-dom";
-import CardDetail from "./CardDetail";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardDetail from './CardDetail';
+import ApprovalCard from './ApprovalCard';
 
 function getTime() {
   return new Date().toLocaleTimeString();
@@ -9,40 +10,87 @@ function getTime() {
 
 //create a function component
 const Clock = () => {
-  return <div className="ui  right floated header">Time is: {getTime()}</div>;
+  return <div className='ui  right floated header'>Time is: {getTime()}</div>;
 };
 
 const App = () => {
   return (
     <div>
-      <div className="ui container">
+      <div className='ui container comments'>
         <Clock />
-        <h2 className="ui left floated header">100DaysofCode</h2>
-        <div className="ui clearing divider"></div>
-        <h2 className="ui center aligned segment header">
+        <h2 className='ui left floated header'>100DaysofCode</h2>
+        <div className='ui clearing divider'></div>
+        <h2 className='ui center aligned segment header'>
           Welcome to the world of REACT!!!!
         </h2>
+        <div className='ui clearing divider'></div>
+        <h2 className='ui left floated header'>Cue cards</h2>
       </div>
-      <div className="ui clearing divider"></div>
-      <h2 className="ui left floated header">Cue cards</h2>
-      <div className="ui container cards">
-        <CardDetail
-          question="What is ReactJS?"
-          answer="ReactJS is a JavaScript  library that helps to build user interface components elegantly"
-        />
-        <CardDetail
-          question="How React renders an element?"
-          answer=" ReactDOM does the process of rendering an element in DOM and importantly it updates only what's necessary."
-        />
-        <CardDetail
-          question="What is JSX?"
-          answer="JSX is special dialect of JavaScript. It is used to create React elements."
-        />
-        <CardDetail
-          question="What is React Component?"
-          answer="Components let you split UI into independent, reusable pieces. 
-                    Basically it is like JavaScript function which accepts input(props) and returns React Elements."
-        />
+      <div className='ui container cards'>
+        <ApprovalCard>
+          <CardDetail
+            question='What is ReactJS?'
+            answer='ReactJS is a JavaScript  library that helps to build user interface components elegantly'
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CardDetail
+            question='How React renders an element?'
+            answer=" ReactDOM does the process of rendering an element in DOM and importantly it updates only what's necessary."
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CardDetail
+            question='What is JSX?'
+            answer='JSX is special dialect of JavaScript. It is used to create React elements.'
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CardDetail
+            question='What is React Component?'
+            answer='Components let you split UI into independent, reusable pieces. 
+                    Basically it is like JavaScript function which accepts input(props) and returns React Elements.'
+          />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <div>
+            <div className='ui small dividing header'>
+              Custom Elements using <i>props.children</i>
+            </div>
+            <form className='ui form'>
+              <div className='field'>
+                <label>First Name</label>
+                <input
+                  type='text'
+                  name='firstName'
+                  placeholder='Enter your first name'
+                ></input>
+              </div>
+              <div className='field'>
+                <label>Last Name</label>
+                <input
+                  type='text'
+                  name='lastName'
+                  placeholder='Enter your last name'
+                ></input>
+              </div>
+              <div className='field'>
+                <div className='ui checkbox'>
+                  <input type='checkbox' tabIndex='0' className='hidden' />
+                  <label>I agree to the Terms and Conditions</label>
+                </div>
+              </div>
+              <button className='ui button' type='submit'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <Clock />
+        </ApprovalCard>
       </div>
     </div>
   );
@@ -50,7 +98,7 @@ const App = () => {
 
 //show it to user
 function tick() {
-  ReactDOM.render(<App />, document.querySelector("#root"));
+  ReactDOM.render(<App />, document.querySelector('#root'));
 }
 
 setInterval(tick, 1000);
