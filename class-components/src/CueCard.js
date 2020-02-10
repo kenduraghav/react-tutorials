7d307847e84c87696517013856027ef0a48cdae0
kenduraghav <kenduraghav@gmail.com>
2020-02-10 18:17:32 +0530

    #100DaysOfCode - Added class based components.

---
+++ class-components/src/CueCard.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const CardDetail = props => {
+  return (
+    <div className='comment'>
+      <div className='content'>
+        <div className='ui dividing header'>{props.question}</div>
+        <div className='description'>
+          <p>{props.answer}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+class CueCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: true,
+      likeCount: 1,
+      className: 'heart blue like icon',
+      favorite: true,
+      favoriteStyle: 'yellow star icon'
+    };
+  }
+
+  onClickLikeIcon = () => {
+    console.log('clicked >>>>' + this.state.liked);
+    this.setState({
+      liked: !this.state.liked,
+      likeCount: this.state.liked ? 1 : 0,
+      className: this.state.liked
+        ? 'heart blue like icon'
+        : 'heart outline like icon'
+    });
+  };
+
+  onClickFavorite = () => {
+    console.log('clicked >>>>' + this.state.favorite);
+    this.setState({
+      favorite: !this.state.favorite,
+      favoriteStyle:
+        this.state.favorite === true ? 'yellow star icon' : 'outline star icon'
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className='ui card'>
+        <div className='content'>
+          <CardDetail
+            question='What is ReactJS?'
+            answer='ReactJS is a JavaScript library that helps to build user interface components elegantly'
+          />
+        </div>
+        <div className='extra content'>
+          <span className='left floated like' onClick={this.onClickLikeIcon}>
+            <i
+              className={this.state.className}
+              onClick={this.onClickLikeIcon}
+            ></i>
+            {this.state.likeCount} Likes
+          </span>
+          <span className='right floated star' onClick={this.onClickFavorite}>
+            <i
+              className={this.state.favoriteStyle}
+              onClick={this.onClickFavorite}
+            ></i>
+            Favorite
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CueCard;
---
+++ class-components/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './Clock';
+import CueCard from './CueCard';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLike: false };
+  }
+
+  onLikeIconClick() {
+    this.setState({
+      isLike: true
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='ui container'>
+          <h2 className='ui left floated header'>100DaysofCode</h2>
+          <h2 className='ui right floated header'>
+            <Clock />
+          </h2>
+
+          <div className='ui clearing divider'></div>
+          <h2 className='ui center aligned segment header'>
+            Welcome to the world of REACT!!!!
+          </h2>
+          <div className='ui clearing divider'></div>
+          <div className='ui cards'>
+            <CueCard />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
