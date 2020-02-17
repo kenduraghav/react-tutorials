e171a9a4e38b4e4389a60c8167a54fa424c6ebea
kenduraghav <kenduraghav@gmail.com>
2020-02-17 20:44:14 +0530

    #100DaysOfCode - Day 16 -Updated dynamic span for image card.

--- pictures/src/api/unsplash.js
+++ pictures/src/api/unsplash.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://api.unsplash.com",
+  baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-      "Client-ID your-client-id"
+      'Client-ID 369309dc8a88dbefbb563f2630f0536660dccee9b9f0ceb96c09acb77eae00b0'
   }
 });
