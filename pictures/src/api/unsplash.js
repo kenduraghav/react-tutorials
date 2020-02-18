fc3f5e7b36a953e14062cb6dde2fe03f6e65f163
kenduraghav <kenduraghav@gmail.com>
2020-02-18 21:00:55 +0530

    Removed Key information

--- pictures/src/api/unsplash.js
+++ pictures/src/api/unsplash.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization:
-      'Client-ID 369309dc8a88dbefbb563f2630f0536660dccee9b9f0ceb96c09acb77eae00b0'
+    Authorization: 'Client-ID  YOUR ACCESS KEY'
   }
 });
